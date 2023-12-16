495cb64f0bcd85d2bd21d8d51904b7a89b936834
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-12-16 18:48:38 +0200

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router  } from '@angular/router';
 //import { HomeComponent } from './home/home.component';
 //import { HomeModule } from './home/home.module';
@@ -7,13 +7,26 @@ import { Router  } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   showContent = true;
   title = "User";
  constructor(
     private router: Router,
   ) { }
+
+  ngOnInit(): void {
+    const currentURL = window.location.href;
+
+// Check if the URL does not contain 'localhost'
+if (!currentURL.includes('localhost')) {
+  // If the URL does not contain 'localhost', find and click a button by its ID
+  const button = document.getElementById('NavigateToSite'); // Replace 'yourButtonId' with your actual button ID
+  if (button) {
+      button.click(); // Click the button if found
+  }
+    }
+  }
 
  navigateToHome(): void {
    this.router.navigate(['/home']); // Navigate to the 'home' route on button click
