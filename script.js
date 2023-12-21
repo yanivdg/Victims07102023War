4bc5ade1cea47c3fd373d58c446eccd7c5080847
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-12-22 00:42:08 +0200

    Update script.js
    link -> href script->src

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ fetch('https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/b
     // Parse the new HTML content
     const parser = new DOMParser();
     const newHTMLDocument = parser.parseFromString(newHTML, 'text/html');
+    const fullPath = 'https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/';
 
-    // Update the base href attribute
-    const baseTag = newHTMLDocument.querySelector('base');
-    if (baseTag) {
-      baseTag.setAttribute('href', '//https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/');
-    }
+    // Update href attribute for link elements
+    newHTMLDocument.querySelectorAll('link[href]').forEach(link => {
+      const href = link.getAttribute('href');
+      if (href) {
+        link.setAttribute('href', fullPath + href);
+      }
+    });
+
+    // Update src attribute for script elements
+    newHTMLDocument.querySelectorAll('script[src]').forEach(script => {
+      const src = script.getAttribute('src');
+      if (src) {
+        script.setAttribute('src', fullPath + src);
+      }
+    });
+    
     const titleTag = newHTMLDocument.querySelector('title');
     titleTag.textContent = 'War Victims 07-Oct-2023 (ver.22122023_0023)';
     // Replace the entire HTML content
