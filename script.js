f7d37a2bbd552d9dbd273057e161a2993f64500b
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-12-22 01:18:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+const fullPath = 'https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/';
+
 fetch('https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/index.html')
   .then(response => response.text())
   .then(newHTML => {
     // Parse the new HTML content
     const parser = new DOMParser();
     const newHTMLDocument = parser.parseFromString(newHTML, 'text/html');
-    const fullPath = 'https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/';
 
     // Update href attribute for link elements
     newHTMLDocument.querySelectorAll('link[href]').forEach(link => {
@@ -21,11 +22,17 @@ fetch('https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/b
         script.setAttribute('src', fullPath + src);
       }
     });
-    
+
     const titleTag = newHTMLDocument.querySelector('title');
-    titleTag.textContent = 'War Victims 07-Oct-2023 (ver.22122023_0023)';
-    // Replace the entire HTML content
-    document.documentElement.innerHTML = newHTMLDocument.documentElement.outerHTML;
+    titleTag.textContent = 'War Victims 07-Oct-2023 (ver.22122023_0119)';
+
+    // Manipulate specific elements before appending to the existing document
+    const newHead = newHTMLDocument.head.innerHTML;
+    const newBody = newHTMLDocument.body.innerHTML;
+
+    // Replace specific elements or append them to the existing document
+    document.head.innerHTML = newHead;
+    document.body.innerHTML = newBody;
   })
   .catch(error => {
     console.error('Error fetching or updating HTML:', error);
