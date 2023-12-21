75de2864947b1b8fde1b4ad6ac63f12f73629aa9
yanivdg <134487944+yanivdg@users.noreply.github.com>
2023-12-22 00:25:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 fetch('https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/index.html')
   .then(response => response.text())
-  .then(htmlContent => {
-    // Manipulate the HTML content
-    const modifiedContent = htmlContent.replace(/(href|src)="([^"]*)"/g, (match, attribute, value) => {
-      // Assuming your files are in the same directory as the HTML file
-      const fullPath = 'https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/' + value; // Replace 'full_path/' with your actual path
-      return `${attribute}="${fullPath}"`;
-    });
+  .then(newHTML => {
+    // Parse the new HTML content
+    const parser = new DOMParser();
+    const newHTMLDocument = parser.parseFromString(newHTML, 'text/html');
 
-    // Create a new HTML document
-    const newHTMLDocument = new DOMParser().parseFromString(modifiedContent, 'text/html');
-
-    // Replace the entire content of the current document body with the modified HTML content
-    document.body.innerHTML = newHTMLDocument.documentElement.outerHTML;
+    // Update the base href attribute
+    const baseTag = newHTMLDocument.querySelector('base');
+    if (baseTag) {
+      baseTag.setAttribute('href', '//https://cdn.jsdelivr.net/gh/yanivdg/Victims07102023War@main/dist/my-app/browser/');
+    }
+    const titleTag = newHTMLDocument.querySelector('title');
+    titleTag.textContent = 'War Victims 07-Oct-2023 (ver.22122023_0023)';
+    // Replace the entire HTML content
+    document.documentElement.innerHTML = newHTMLDocument.documentElement.outerHTML;
   })
   .catch(error => {
-    console.error('Error fetching HTML:', error);
+    console.error('Error fetching or updating HTML:', error);
   });
