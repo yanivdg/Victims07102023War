b0af3c805f2eb99293df5011e8495e78835772cd
yanivdg <134487944+yanivdg@users.noreply.github.com>
2024-01-03 19:37:28 +0000

    adjust mobile

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,25 @@
+#!/bin/bash
+
+echo "Starting Git commands..."
+
+# Check if a commit message was provided
+if [ $# -eq 0 ]; then
+    echo "Please provide a commit message."
+    exit 1
+fi
+
+commit_message="$1"
+
+echo "Running: git status"
 git status
-git add .
-git commit -m "fix for mobile"
+
+echo "Running: git add ."
+git add --all # To add all changes including untracked files
+
+echo "Running: git commit -m 'push files: $commit_message'"
+git commit -m "push files: $commit_message"
+
+echo "Running: git push origin main"
 git push origin main
+
+echo "Git commands execution completed."
