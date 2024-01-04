7b666183291ae653c0ad2abb7c2d0fbbaff37790
yanivdg <134487944+yanivdg@users.noreply.github.com>
2024-01-04 23:15:47 +0000

    add file

--- src/app/app-youtube-player/app-youtube-player.component.ts
+++ src/app/app-youtube-player/app-youtube-player.component.ts
@@ -1,33 +1,63 @@
-import { Component, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit,AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
     selector: 'app-youtube-player',
     templateUrl: './app-youtube-player.component.html',
     styleUrl: './app-youtube-player.component.css'
 })
 
-export class AppYoutubePlayerComponent implements AfterViewInit {
+export class AppYoutubePlayerComponent implements OnInit,AfterViewInit {
     width: number | undefined;
     height: number | undefined;
   
-    constructor(private el: ElementRef) {}
-
+    constructor(private el: ElementRef,private route: ActivatedRoute) {}
+     playerVars:any;
+    player: YT.Player | null = null;  // Initialize to null
     videoId = '9eDzojXuMZY'; // replace with your video ID
-    playerVars = {
-        controls: 0,  // This disables the player controls
-        loop: 1,      // This enables looping of the video
-        autoplay: 1,  // This enables autoplay of the video
-        disablekb: 1,  // This disables keyboard controls
-        mute: 1,
-        playlist: this.videoId  // This sets the playlist to the same video ID
-    };
+     
+    
+
+    ngOnInit():void{
+        // Retrieving parameters in the child component
+        this.route.queryParams.subscribe(params => {
+          const userchoice = params['mute'];
+          if(userchoice > 0)
+            this.playVideoSound();
+          else this.cancelPlayback();
+        });
+    }
     ngAfterViewInit() {
-        const div = this.el.nativeElement.querySelector('.youtubewrap');
+         const div = this.el.nativeElement.querySelector('.youtubewrap');
         const computedStyle = window.getComputedStyle(div);
   
         this.width = parseInt(computedStyle.width.replace('px', ''), 10)
         this.height = parseInt(computedStyle.height.replace('px', ''), 10)
       }
+      onPlayerReady(event: YT.PlayerEvent) {
+        this.player = event.target as YT.Player;  // Type assertion
+      }
+      
+      playVideoSound() {
+        // Unmute the player:
+        this.playerVars = {
+          controls: 0,  // This disables the player controls
+          loop: 1,      // This enables looping of the video
+          autoplay: 1,  // This enables autoplay of the video
+          disablekb: 1,  // This disables keyboard controls
+          mute: 1,
+          playlist: this.videoId  // This sets the playlist to the same video ID
+      };
+      this.player?.playVideo();
+      }      
+      cancelPlayback(){
+                // Mute the player:
+                this.playerVars = {
+                  controls: 0,  // This disables the player controls
+                  loop: 1,      // This enables looping of the video
+                  autoplay: 1,  // This enables autoplay of the video
+                  disablekb: 1,  // This disables keyboard controls
+                  mute:0,
+                  playlist: this.videoId  // This sets the playlist to the same video ID
+              };
+      }
 }
-
-//https://www.youtube.com/watch?v=4iEq2hUZMMQ
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,15 +18,20 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
  // Check if the URL does not contain 'localhost'
       if (!window.location.href.includes('localhost')) {
-  // If the URL does not contain 'localhost', find and click a button by its ID
-  this.navigateToHome();
-  }
-  }
+        // If the URL does not contain 'localhost', find and click a button by its ID
+        this.navigateToHome();
+       }
+    }
 
  navigateToHome(): void {
-   this.router.navigate(['/home']); // Navigate to the 'home' route on button click
-   this.showContent = false;
-
-}
+    let queryParams = { mute: 1 };
+    const confirmed = window.confirm('The Page involved videos with sound, do you agree to proceed with sound in the background?');
+    if (confirmed) {
+        // Use the YouTube player API to mute the player
+        queryParams = { mute: 0 };
+      }
+    this.router.navigate(['/home'],{queryParams,skipLocationChange: true }); // Navigate to the 'home' route on button click
+    this.showContent = false;
+  }
 }
 
